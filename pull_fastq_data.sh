3f4b9e38971ab2b17372accd64a5deffd5f9d79a
Nick-Eagles <nick.eagles@libd.org>
2020-07-23 13:28:14 -0400

    Begin the script for downloading example FASTQ files: so far a complete but untested procedure for pulling DLPFC samples from synapse is in place

---
+++ pull_fastq_data.sh
@@ -0,0 +1,20 @@
+#  Run this script from the main 'SPEAQeasy-example' folder, specifying an
+#  output folder to place downloaded files:
+#
+#  bash pull_fastq_data.sh "/some_directory"
+
+if [ "$1" == "" ]; then
+    echo "Please provide a download location for the FASTQ data when running this script."
+    echo -e '\ne.g. bash pull_fastq_data.sh "/some_directory"'
+    exit 1
+fi
+
+dest_dir=$1
+
+#  Get the IDs for samples we are using in the example
+spec_ids=$(cut -f 5 sample_selection/samples.manifest | cut -d "_" -f 1 | uniq)
+
+#  Pull those samples from synapse
+for specimen in $spec_ids; do
+    synapse get --downloadLocation $dest_dir -q "select * from syn8408214 where specimenID='$specimen' AND fileFormat='fastq'"
+done
