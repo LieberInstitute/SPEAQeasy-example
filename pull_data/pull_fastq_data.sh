1475a61e8c6f5010604561d0ca38170545078b4f
Nick-Eagles <nick.eagles@libd.org>
2020-08-10 12:24:46 -0400

    Add script for making a manifest for the pulled data; write out the RMD guide for downloading example data

--- pull_data/pull_fastq_data.sh
+++ pull_data/pull_fastq_data.sh
@@ -18,3 +18,6 @@ spec_ids=$(cut -f 5 sample_selection/samples.manifest | cut -d "_" -f 1 | uniq)
 for specimen in $spec_ids; do
     synapse get --downloadLocation $dest_dir -q "select * from syn8408214 where specimenID='$specimen' AND fileFormat='fastq'"
 done
+
+#  Create the 'samples.manifest' file for those who want to run SPEAQeasy
+Rscript -d "$dest_dir"
